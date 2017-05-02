51d0a8046497164009d7ecb350c6f58f91e8dcd2
mjmayer <mjmayer@gmail.com>
2017-05-02 08:20:26 -0700

    Added warning and critical variables Added variables to comply with nagios check standards. Removed UCDavis bigfix url

--- check_bigfix_webreports.py
+++ check_bigfix_webreports.py
@@ -9,13 +9,16 @@ parser = argparse.ArgumentParser(add_help=False)
 parser.add_argument('-bigfix', '--bigfixlogin',
                     help="BigFix Login ID (Need to escape '\\' by using ''\\\\''")
 parser.add_argument('-P', '--bigfixpass', help='BigFix Password/Passphrase')
-parser.add_argument('-C', '--computers',
-                    help='Number of computers to look for in the past timeframe')
+parser.add_argument('-c', '--computers',
+                    help='Number of computers to look for in the past timeframe', type=int)
 parser.add_argument('-M', '--minutes', help='Number of minutes to look for last checkin',
-                    default='60')
+                    default='60', type=int)
+parser.add_argument('-U', '--url', help='BigFix API URL')
+parser.add_argument('-W', '--warn', help='Threshold for warning', type=int)
+parser.add_argument('-C', '--critical', help='Threshold for critical', type=int)
 args = parser.parse_args()
 
-BIGFIX_API_URL = 'https://bigfix.ucdavis.edu:52311/api/'
+BIGFIX_API_URL = args.url
 
 #open bigfix session
 bigfix_session = BigFix(BIGFIX_API_URL, args.bigfixlogin, args.bigfixpass)
@@ -24,7 +27,7 @@ bigfix_session.authenticate()
 #changes the timeframe to look at
 params = {
     'relevance' : 'number of elements of set of ((values whose (it as time > '
-                  '(now - ' + args.minutes + '*minute)) of it) '
+                  '(now - ' + str(args.minutes) + '*minute)) of it) '
                   'of results of bes properties whose (Name of it = "Last Report Time"))'
     }
 #query bigfix
@@ -33,13 +36,13 @@ xml_result = xml_result.encode('utf-8')
 root = etree.fromstring(xml_result)
 result = [ x.text for x in root[0][0].findall('Answer')][0]
 result = int(result)
-if result > 100:
+if result >= args.warn:
     print("OK - %s BES Computers reported" % result)
     sys.exit(0)
-elif result < 100 and result > 50:
+elif result < args.warn and result > args.critical:
     print("WARNING - %s BES Computers reported." % result)
     sys.exit(1)
-elif result < 50:
+elif result <= args.critical:
     print("CRITICAL - %s BES Computers reported." % result)
     sys.exit(2)
 else:
